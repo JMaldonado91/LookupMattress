4001f99719f694d4d7d8b9f62e905f48afb10094
Jorleni Maldonado <jorlenim@outlook.com>
2019-03-18 18:37:38 -0400

    fixing mysql

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
-using MySql.Data.MySqlClient;
 using System.Data.SqlClient;
 using System.Data.SQLite;
 using System.Windows.Forms;
